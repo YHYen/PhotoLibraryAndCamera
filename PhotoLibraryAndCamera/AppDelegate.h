b0863ebfd13942c1290e0a7d7c4ce81bbd6aa8a9
YHYen <cloudyhyen@gmail.com>
2023-04-08 14:54:49 +0800

    Initial Commit

---
+++ PhotoLibraryAndCamera/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PhotoLibraryAndCamera
+//
+//  Created by 顏逸修 on 2023/4/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PhotoLibraryAndCamera/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PhotoLibraryAndCamera
+//
+//  Created by 顏逸修 on 2023/4/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
